8caf6bda187e415197e1cd12ad81c9b7842a2f39
MartinHalus <halus.martin@gmail.com>
2021-07-17 16:33:06 +0200

    added sounds works in emulator, not in phone

--- app/src/main/java/com/maha/calpar/MainActivity.kt
+++ app/src/main/java/com/maha/calpar/MainActivity.kt
@@ -1,11 +1,102 @@
 package com.maha.calpar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.SoundPool
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var soundPool: SoundPool? = null
+    private var sound1 = 0
+    private var sound2 = 0
+    private var sound3 = 0
+    private var sound4 = 0
+    private var sound3StreamId = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        soundPool = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+            SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(audioAttributes)
+                .build()
+        } else {
+            SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+        }
+
+        sound1 = soundPool!!.load(this, R.raw.snd_white_noise_01, 1)
+        sound2 = soundPool!!.load(this, R.raw.snd_pink_noise_01, 1)
+        sound3 = soundPool!!.load(this, R.raw.snd_heart_beat_01, 1)
+        sound4 = soundPool!!.load(this, R.raw.snd_brown_noise_01, 1)
+
     }
+
+    fun playSound(v: View) {
+        when (v.id) {
+            R.id.btnWhiteNoise -> {
+                sound3StreamId = soundPool!!.play(sound1, 1f, 1f, 0, 0, 1f)
+                soundPool!!.pause(sound3StreamId)
+                Toast.makeText(this, "playing: White noise", Toast.LENGTH_SHORT).show()
+
+            }
+
+            R.id.btnPinkNoise -> {
+                sound3StreamId = soundPool!!.play(sound2, 1f, 1f, 0, 0, 1f)
+                soundPool!!.pause(sound3StreamId)
+                Toast.makeText(this, "playing: Pink noise", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.btnHeartBeat -> {
+                sound3StreamId = soundPool!!.play(sound3, 1f, 1f, 0, 0, 1f)
+                soundPool!!.pause(sound3StreamId)
+                Toast.makeText(this, "playing: Heart beat", Toast.LENGTH_SHORT).show()
+            }
+            R.id.btnBrownNoise -> {
+                sound3StreamId = soundPool!!.play(sound4, 1f, 1f, 0, 0, 1f)
+                soundPool!!.pause(sound3StreamId)
+                Toast.makeText(this, "playing: Brown noise", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun stopSounds(v: View) {
+
+
+        soundPool!!.stop(sound3StreamId)
+    }
+
+
+
+
 }
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
