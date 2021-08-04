32e2b70ab258d64215e32c8cfcaaedc73cc1d010
MartinHalus <halus.martin@gmail.com>
2021-08-04 22:05:11 +0200

    in progress - before consultation

--- app/src/main/java/com/maha/calpar/MainActivity.kt
+++ app/src/main/java/com/maha/calpar/MainActivity.kt
@@ -10,8 +10,11 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.ActionBarContextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.snackbar.Snackbar.make
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,13 +26,24 @@ class MainActivity : AppCompatActivity() {
     private var sound3 = 0
     private var sound4 = 0
     private var sound3StreamId = 0
-    lateinit var snackText:String
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        btnWhiteNoise.setOnClickListener {
+//            // if button is already in selected state and now it is pressed
+//            // again,then it will reach in not selected state and vice versa
+//            btnWhiteNoise.isSelected != btnWhiteNoise.isSelected
+//        }
+
+//        buttonSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+//            // if the switch is checked,then enable the button,else not
+//            button.isEnabled = isChecked
+//        }
 
 
         soundPool = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -50,58 +64,63 @@ class MainActivity : AppCompatActivity() {
         sound3 = soundPool!!.load(this, R.raw.snd_heart_beat_01, 1)
         sound4 = soundPool!!.load(this, R.raw.snd_brown_noise_01, 1)
 
-    }
 
-    fun playSound(v: View) {
-        when (v.id) {
-            R.id.btnWhiteNoise -> {
-                sound3StreamId = soundPool!!.play(sound1, 1f, 1f, 0, -1, 1f)
-                loadSnack(v)
+
+        toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
+
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btnWhiteNoise -> {
+
+                        sound3StreamId = soundPool!!.play(sound2, 1f, 1f, 0, -1, 1f)
+                        showToast("White toast")
+                    }
+
+                    R.id.btnPinkNoise -> {
+                        stopSounds(v = View(this))
+                        sound3StreamId = soundPool!!.play(sound3, 1f, 1f, 0, -1, 1f)
+                        showToast("Pink toast.")
+                    }
+
+                    R.id.btnBrownNoise -> {
+                        sound3StreamId = soundPool!!.play(sound4, 1f, 1f, 0, -1, 1f)
+                        loadSnack("2")
+
+                    }
+                }
+            } else {
+                if (toggleButtonGroup.checkedButtonId == View.NO_ID) {
+                    showToast("Nothing Selected")
+                }
             }
-
-            R.id.btnPinkNoise -> {
-                sound3StreamId = soundPool!!.play(sound2, 1f, 1f, 0, -1, 1f)
-                loadSnack(v)
-            }
-
-            R.id.btnHeartBeat -> {
-                sound3StreamId = soundPool!!.play(sound3, 1f, 1f, 0, -1, 1f)
-                loadSnack(v)
-            }
-
-            R.id.btnBrownNoise -> {
-                sound3StreamId = soundPool!!.play(sound4, 1f, 1f, 0, -1, 1f)
-                loadSnack(v)
-            }
-
-            R.id.btnToastTest -> {
-
-                Toast.makeText(this, "playing: toast test", Toast.LENGTH_SHORT).show()
-            }
-
         }
+
     }
 
 
-    fun stopSounds(v: View) {
 
 
+
+    private fun stopSounds(v: View) {
+
+//        btnDefaultDesign(v)
         soundPool?.stop(sound3StreamId)
     }
 
-    fun loadTest(v: View) {
-
-
-        Toast.makeText(this, "playing: toast test", Toast.LENGTH_SHORT).show()
+    fun showToast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 
 
-    fun loadSnack(v: View) {
 
-        var snackText = "NEED TO FIGURE IT OUT, HOW TO CHANGE ACCORDING TO CLICKED BUTTON"
 
-        val snackbar = make(v, snackText, 1500)
-        //snackbar.anchorView = v
+
+    fun loadSnack(str: String) {
+
+//        var snackText = "NEED TO FIGURE IT OUT, HOW TO CHANGE ACCORDING TO CLICKED BUTTON"
+
+        val snackbar = make(ConstraintLayout(this), str, 1500)
+
 
         val view: View = snackbar.view
         val params = view.layoutParams as FrameLayout.LayoutParams
@@ -112,8 +131,18 @@ class MainActivity : AppCompatActivity() {
                 snackbar.show()
     }
 
-    
 
+//    private fun btnDefaultDesign(v: View) {
+//
+//        btnWhiteNoise.setBackgroundResource(R.drawable.bg_button_2)
+//        btnBrownNoise.setBackgroundResource(R.drawable.bg_button_2)
+//        btnHeartBeat.setBackgroundResource(R.drawable.bg_button_2)
+//        btnPinkNoise.setBackgroundResource(R.drawable.bg_button_2)
+//        btnSnackBar.setBackgroundResource(R.drawable.bg_button_2)
+//        btnWhiteNoise.setBackgroundResource(R.drawable.bg_button_2)
+//
+//
+//    }
 
 
 
