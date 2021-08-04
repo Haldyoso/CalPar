d80f964fea79e6d83d4cab158f2d4e1e37e4a714
MartinHalus <halus.martin@gmail.com>
2021-08-05 00:35:36 +0200

    in progress - after consultation

--- app/src/main/java/com/maha/calpar/MainActivity.kt
+++ app/src/main/java/com/maha/calpar/MainActivity.kt
@@ -10,23 +10,34 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.ActionBarContextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.snackbar.Snackbar.make
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
 
-    private var soundPool: SoundPool? = null
+    private val soundPool: SoundPool by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+            SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(audioAttributes)
+                .build()
+        } else {
+            SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+        }
+    }
+
     private var sound1 = 0
     private var sound2 = 0
     private var sound3 = 0
     private var sound4 = 0
     private var sound3StreamId = 0
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,46 +57,36 @@ class MainActivity : AppCompatActivity() {
 //        }
 
 
-        soundPool = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val audioAttributes = AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                .build()
-            SoundPool.Builder()
-                .setMaxStreams(1)
-                .setAudioAttributes(audioAttributes)
-                .build()
-        } else {
-            SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+        sound1 = soundPool.load(this, R.raw.snd_white_noise_01, 1)
+        sound2 = soundPool.load(this, R.raw.snd_pink_noise_01, 1)
+        sound3 = soundPool.load(this, R.raw.snd_heart_beat_01, 1)
+        sound4 = soundPool.load(this, R.raw.snd_brown_noise_01, 1)
+
+        btnStop.setOnClickListener {
+            stopSounds()
         }
-
-        sound1 = soundPool!!.load(this, R.raw.snd_white_noise_01, 1)
-        sound2 = soundPool!!.load(this, R.raw.snd_pink_noise_01, 1)
-        sound3 = soundPool!!.load(this, R.raw.snd_heart_beat_01, 1)
-        sound4 = soundPool!!.load(this, R.raw.snd_brown_noise_01, 1)
-
-
 
         toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
 
+            stopSounds()
             if (isChecked) {
-                when (checkedId) {
+                sound3StreamId = when (checkedId) {
                     R.id.btnWhiteNoise -> {
-
-                        sound3StreamId = soundPool!!.play(sound2, 1f, 1f, 0, -1, 1f)
                         showToast("White toast")
+                        soundPool.play(sound2, 1f, 1f, 0, -1, 1f)
                     }
 
                     R.id.btnPinkNoise -> {
-                        stopSounds(v = View(this))
-                        sound3StreamId = soundPool!!.play(sound3, 1f, 1f, 0, -1, 1f)
                         showToast("Pink toast.")
+                        soundPool.play(sound3, 1f, 1f, 0, -1, 1f)
                     }
 
                     R.id.btnBrownNoise -> {
-                        sound3StreamId = soundPool!!.play(sound4, 1f, 1f, 0, -1, 1f)
                         loadSnack("2")
-
+                        soundPool.play(sound4, 1f, 1f, 0, -1, 1f)
+                    }
+                    else -> {
+                        error("Unknown noise")
                     }
                 }
             } else {
@@ -97,38 +98,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-
-
-
-    private fun stopSounds(v: View) {
-
-//        btnDefaultDesign(v)
-        soundPool?.stop(sound3StreamId)
+    private fun stopSounds() {
+        soundPool.stop(sound3StreamId)
     }
+
 
     fun showToast(str: String) {
         Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 
 
+    private fun loadSnack(str: String) {
 
 
-
-    fun loadSnack(str: String) {
-
-//        var snackText = "NEED TO FIGURE IT OUT, HOW TO CHANGE ACCORDING TO CLICKED BUTTON"
-
-        val snackbar = make(ConstraintLayout(this), str, 1500)
-
-
-        val view: View = snackbar.view
-        val params = view.layoutParams as FrameLayout.LayoutParams
-        params.gravity = Gravity.TOP
-        view.layoutParams = params
-
-
-                snackbar.show()
+        Snackbar.make(root, str, 1500)
+            .apply {
+                (view.layoutParams as FrameLayout.LayoutParams).gravity = Gravity.TOP
+            }
+            .show()
     }
 
 
@@ -143,10 +130,6 @@ class MainActivity : AppCompatActivity() {
 //
 //
 //    }
-
-
-
-
 
 
 }
